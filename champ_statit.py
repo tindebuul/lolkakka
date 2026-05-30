a83e2b862106a08071f2dc2d9ba29dc02c92c469
tindebuul <tiitus.ogata@gmail.com>
2026-05-30 17:27:36 +0300

    Erikoismerkit ja välit poistettu

--- champ_statit.py
+++ champ_statit.py
@@ -3,6 +3,3 @@ import webbrowser
 def open_analytics(champ):
 
     webbrowser.open(f"https://lolalytics.com/lol/{champ}/build/")
-
-    
-        
--- main.py
+++ main.py
@@ -2,8 +2,15 @@ from champ_statit import open_analytics
 
 champ = ""
 
+kielletty_merkki = ["'", "1"]
+
 while (True):
         champ = input("Ketä pelaat: ")
+        
+        champ = champ.strip().replace(" ", "")
+                                      
+        for i in kielletty_merkki:
+            champ = champ.replace(i, "")
 
         if champ == "exit":
             break
